5de599347b3571241d26fcf3a0b15975e94d9881
bryanfoley <bryanfoley@users.noreply.github.com>
2016-10-28 12:33:09 +0200

    Added files
    Add directories

---
+++ OrderSystem/Model.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OrderSystem
+{
+	class Model :EventArgs          //This is a class which models the interactions of ACME, the Manufacturer and 20 Customers over 365 days.
+	{
+		public int DayCount = 0;
+		public int YearCount = 0;
+		public int day = 0;
+		public int LowerLimit;
+
+		public static event ACMEHandler EventACME;
+		public static event CustomerHandler EventCUS;
+		public static event ManufacturerHandler EventMAN;
+		public static event CustomerHandler EventCancelOrder;
+
+
+		bool OpenForBusiness = true;
+
+		Random r = new Random();
+		Random rand = new Random();
+
+		CustomerEventArgs[] Customers = new CustomerEventArgs[20];
+		ManufacturerEventArgs b = new ManufacturerEventArgs();
+		ACMEEventArgs a = new ACMEEventArgs();
+
+		public void Run(int StepSize, int ReorderPoint)   //Default Constructor
+		{
+			StreamWriter sw1 = new StreamWriter(@"/home/bryan/Profits.txt",true);
+			StreamWriter sw2 = new StreamWriter(@"/home/bryan/Stock.txt",true);
+			StreamWriter sw3 = new StreamWriter(@"/home/bryan/DataPoints.xls", true);
+
+			YearCount = 0;
+
+			OpenForBusiness = true;
+			a.StdOrder = StepSize;
+			LowerLimit = ReorderPoint;
+
+
+			EventCUS += new CustomerHandler(a.HeardIt);
+			EventACME += new ACMEHandler(b.HeardIt);
+			EventMAN += new ManufacturerHandler(a.UpdateStock);
+			//EventCancelOrder += new CustomerHandler(a.CancelOrder);
+
+			for (day = 1; day <= 365; day++)
+			{
+				Console.WriteLine("\n\t\t Day {0}", day);
+				DayCount = 0;
+				if (a.stock <= 0 && (day < b.ShipDay))
+				{
+					Console.WriteLine("NO ORDERS UNTIL THE STOCK IS REPLENISHED");
+					OpenForBusiness = false;
+				}
+				else if (((a.stock <= LowerLimit) && (day > b.ShipDay)))
+				{
+					OpenForBusiness = true;
+					b.CalcLeadTime(day, r);
+					GenReorder(day, r);
+				}
+				else if (day == b.ShipDay)
+				{
+					Console.WriteLine("We are open for Busniness again.");
+					OpenForBusiness = true;
+					GenShipment();
+				}
+				if (OpenForBusiness == true)
+				{
+					for (int d = 0; d < Customers.Length; d++)
+					{
+						if ((rand.NextDouble()) < (31.0 / 365.0))
+						{
+							Console.WriteLine("Customer Number {0} ordered a GAD", d + 1);
+							GenOrders(day, r);
+							if (a.stock < a.OrderSize)
+							{
+								Console.WriteLine("This order cannot be filled.");
+								OpenForBusiness = false;
+								//break;
+							}
+							else
+							{
+								OpenForBusiness = true;
+								YearCount++;
+								DayCount++;
+							}
+						}
+					}
+					if (YearCount >= 600)
+
+						//OpenForBusiness = false;
+						break;
+
+				}
+				Console.WriteLine("The Number of orders on day {0} is {1}", day, DayCount);
+				a.profit =a.profit - (0.30 * a.stock);
+
+				//sw1.WriteLine("{0}\t{1:#.###e+00}", day, a.profit);
+				//sw2.WriteLine("{0}\t{1}", day, a.stock);
+			}
+			Console.WriteLine("The total number of orders for the year is {0}", YearCount);
+			//sw1.WriteLine("{0}\t{1}\t{2}\t{3}", a.StdOrder, LowerLimit, YearCount, a.profit);
+			if (a.profit > 1.21e6 && YearCount == 600)
+			{
+				sw3.WriteLine("{0}\t{1}\t{2}\t{3:#.###e+00}", a.StdOrder, LowerLimit, YearCount, a.profit);
+				Console.WriteLine("{0}\t{1}\t{2}\t{3:#.###e+00}", a.StdOrder, LowerLimit, YearCount, a.profit);
+			}
+			Console.WriteLine("{0}\t{1}\t{2}\t{3}", a.profit,b.ShipDay,b.OrderDay, a.profit);
+			sw1.Close();
+			sw2.Close();
+			sw3.Close();
+		}
+		public void Reset()
+		{
+			a.profit = 0;
+			a.stock = 9000;
+			b.ShipDay = 0;
+			a.profit = 0;
+			a.OrderSize = 0;
+			b.LeadTime = 0;
+			b.OrderDay = 0;
+			a.OrderSize = 0;
+			a.StdOrder = 0;
+			OpenForBusiness = true;
+		}
+		//Events
+		public static void GenOrders(int h, Random r)
+		{
+			CustomerEventArgs e1 = new CustomerEventArgs(h, r);
+			OnEventCus(e1);
+		}
+		public static void OnEventCus(CustomerEventArgs e)
+		{
+			if (EventCUS != null)
+			{
+				EventCUS(new object(), e);
+			}
+		}
+		public static void GenReorder(int s, Random r)
+		{
+			ACMEEventArgs e1 = new ACMEEventArgs();
+			OnEventACME(e1);
+		}
+		public static void OnEventACME(ACMEEventArgs e)
+		{
+			if (EventACME != null)
+			{
+				EventACME(new object(), e);
+			}
+		}
+		public static void OnEventMAN(ManufacturerEventArgs e)
+		{
+			if (EventMAN != null)
+			{
+				EventMAN(new object(), e);
+			}
+		}
+		public static void GenShipment()
+		{
+			ManufacturerEventArgs e1 = new ManufacturerEventArgs();
+			OnEventMAN(e1);
+		}
+		public static void OnEventShipment(ManufacturerEventArgs e)
+		{
+			if (EventMAN != null)
+			{
+				EventMAN(new object(), e);
+			}
+		}
+		public static void CancelOrder()
+		{
+			CustomerEventArgs e1 = new CustomerEventArgs();
+			OnEventCancelOrder(e1);
+		}
+		public static void OnEventCancelOrder(CustomerEventArgs e)
+		{
+			if (EventCancelOrder != null)
+			{
+				EventCancelOrder(new object(), e);
+			}
+		}
+	}
+}
---
+++ OrderSystem/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrderSystem
+{
+	public delegate void CustomerHandler(object o, CustomerEventArgs e);
+	public delegate void ACMEHandler(object o, ACMEEventArgs e);
+	public delegate void ManufacturerHandler(object o, ManufacturerEventArgs e);
+
+	public class CustomerEventArgs : EventArgs      //Customer class
+	{
+		public int OrderNum=0;                      //Size of the customer order
+
+		public int RandomNumber(int min, int max, int day, Random r)
+		{
+			return (100 + (day / 365 * (200 - 100)) + r.Next(-10,10));  //OrderNum increaes as the year goes by
+		}
+
+		public CustomerEventArgs(int f, Random r)
+		{
+			OrderNum = RandomNumber(-10, 10, f, r); //Randomly assign the OrderNum
+		}
+		public CustomerEventArgs()      //Default Constructor
+		{
+
+		}
+	}
+
+	public class ACMEEventArgs : EventArgs          //ACME class
+	{
+		public int stock = 9000;                    //Initial Stock level
+		public int StdOrder;                        //Amount to be reordered
+		public double profit=0;                     //Initial Profit
+		public int OrderSize=0;                     //The number of GAD a customer ordered
+
+		public void HeardIt(object o, CustomerEventArgs e)
+		{//When a customer places an order, ACME can either fill it or do not have enough stock to fill it.
+			//Some messages to the screen have been disabled but, if you wish to see every transaction for every
+			//day of the year, then uncomment them.
+			OrderSize = e.OrderNum;
+			if (OrderSize > stock)
+			{
+				//Console.WriteLine("We do not have enough stock to fill this order.");
+				//profit -= 0.30*stock;
+			}
+			else
+			{
+				//Console.WriteLine("The Customer ordered {0} GAD", OrderSize);
+				//Console.WriteLine("The remaining stock is {0}", stock - OrderSize);
+
+				profit += ((OrderSize * 25));   //Calculate the profit from as successful order
+
+				//Console.WriteLine("The Profits are: {0}\n", profit);
+				stock -= OrderSize;
+			}
+		}
+		public void UpdateStock(object o, ManufacturerEventArgs e)
+		{//When a new shipment arrives, the stock is updated on that day.
+			stock += StdOrder;
+			//Console.WriteLine("->A new shipment arrived! The stock level is now {0}",stock);
+		}
+	}       
+
+	public class ManufacturerEventArgs : EventArgs  //Manufacturer class
+	{
+		public int LeadTime;    //The number of days to fill a new shipment to ACME
+		public int ShipDay;     //The day the shipment will arrive at ACME
+		public int OrderDay;    //The day the order was first placed
+
+		public ManufacturerEventArgs(int day, Random r)
+		{
+
+		}
+		public ManufacturerEventArgs()
+		{
+
+		}
+
+		public int RandomNumber(int day, Random r)
+		{
+			return (25 + r.Next(-10, 10));      //Random number generator
+		}
+		public void CalcLeadTime(int day, Random r)
+		{
+			OrderDay = day;                     
+			LeadTime = (RandomNumber(day, r));  //Randomly calculate the LEadTime
+			ShipDay = OrderDay + LeadTime;      //Determine when to dispatch the new shipment to ACME
+		}
+		public void HeardIt(object o, ACMEEventArgs e)
+		{//When ACME places an order for a new shipment
+			//and that shipment can be delivered before the end of the year,
+			//the order fee of 2000 is taken away on that day. Or else, no fee is charged if the
+			//shipment will not arrive before the end of the year.
+			if (ShipDay > 365)
+			{
+				//Console.WriteLine("The shipment will not arrive before the end of the year!");
+				//Console.WriteLine("The shipment has been cancelled and the order fee has not been deducted.");
+			}
+			else
+			{
+				//Console.WriteLine("We are sending you {0} GAD in {1} days!!!",e.StdOrder, LeadTime);
+				e.profit -= 2000.0;
+			}
+		}
+	}
+
+	class Program
+	{
+		public static int i;
+		public static int j;
+
+		static void Main(string[] args)
+		{
+			for (j = 1000; j <= 10000; j += 100)
+			{
+				for (i = 1000; i <= 10000; i += 100)
+				{
+					Model z = new Model();      //Creae a new Model of the ACME/Manufacturer/Customer environment
+					z.Run(i,j);                 //Run the sim.
+				}
+			}
+			/*StreamReader sr1 = new StreamReader(@"Z:\DataPoints.xls");
+            while ((line = sr1.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }*/
+		}
+	}
+}
